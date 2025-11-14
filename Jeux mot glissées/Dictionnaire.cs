95029bcc46aba6325b893d83219f5b1ffb8e87f9
Clemdo140 <doatclement@icloud.com>
2025-11-14 18:23:20 +0100

    18h20

--- Jeux mot glissées/Dictionnaire.cs
+++ Jeux mot glissées/Dictionnaire.cs
@@ -8,5 +8,34 @@ namespace Jeux_mot_glissées
 {
     internal class Dictionnaire
     {
+        List<List<string>> mots;
+        string langue;
+
+        public Dictionnaire(List<List<string>> mots, string langue)
+        {
+            this.mots = mots;
+            this.langue = langue;
+        }
+        public List<List<string>> Mots
+        {
+            get { return mots; }
+        }
+        public string Langue
+        {
+            get { return langue; }
+        }
+
+        public string toString()
+        {
+            return ($"mots : {this.mots} \nlangue : {this.langue} ");
+        }
+        public bool RechDichoRecursif(string mot)
+        {
+            return false;
+        }
+        public void Tri_XXX()
+        {
+
+        }
     }
 }
