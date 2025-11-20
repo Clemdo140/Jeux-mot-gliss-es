0b3c9cc682f28fa125f73577739089ce1c0d9e9a
Clemdo140 <doatclement@icloud.com>
2025-11-20 23:56:02 +0100

    minuit

--- Jeux mot glissées/Joueur.cs
+++ Jeux mot glissées/Joueur.cs
@@ -28,34 +28,45 @@ namespace Jeux_mot_glissées
         public List<string> Motstrouvés
         {
             get { return motstrouvés; }
+            private set {; }
         }
         public int Scoretot
         {
             get { return scoretot; }
+            private set {; }
         }
         public List<int> Scoreplateau
         {
             get { return scoreplateau; }
+            private set {; }
+
         }
 
         public void Add_Mot(string mot)
         {
+            if (mot != null && mot.Length != 0 && !Contient(mot)) //on vérifie que le mot n'est pas vide et qu'il n'a pas déja été ajouté à la liste
+            {
 
+                Motstrouvés.Add(mot.ToUpper());//on met en majuscules pour éviter les erreurs
+            }
         }
 
         public string toString()
         {
             return ($"nom : {this.nom} \nmotstrouvés : {this.motstrouvés} \nscoretot : {this.scoretot} \nscoreplateau : {this.scoreplateau}");
-        
-       }
+
+        }
         public void Add_Score(int val)
         {
-
+            if (val > 0)
+            {
+                Scoretot += val; // Augmente le score du joueur qui vient de jouer
+            }
         }
 
         public bool Contient(string mot)
         {
-            return false;
-        }
+            return Motstrouvés.Contains(mot.ToUpper()); //on vérifie si le mot en paramètre est dans la liste des most trouvés, on met en majuscules pour éviter les erreurs
+        }
     }
 }
