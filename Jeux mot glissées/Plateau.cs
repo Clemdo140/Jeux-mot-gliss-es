07e18979966ae327683f4cc4b5848dbe6c5003c6
Clemdo140 <doatclement@icloud.com>
2025-11-21 15:12:19 +0100

    15h12

--- Jeux mot glissées/Plateau.cs
+++ Jeux mot glissées/Plateau.cs
@@ -8,37 +8,71 @@ namespace Jeux_mot_glissées
 {
     internal class Plateau
     {
-        string[,] matlettres; //grille de jeu
-        Dictionary<char, Tuple<int, int>> lettrescontraintes; //Stocke les données lues dans Lettres.txt
+        int nblignes = 8;
+        int nbcolonnes = 8;
+        string CHEMIN_LETTRES = "Lettres.txt";
+        char[,] matrice;//le tableau en 2D actuel
+        Random r = new Random();//pour générer aléatoirement le plateau
+        private Dictionary<char, (int max, int poids)> lettrescontraintes; // Dictionnaire interne pour stocker les contraintes de Lettres.txt.
 
-        public Plateau(string[,] matlettres, Dictionary<char, Tuple<int, int>> lettrescontraintes)
+        public Plateau()   //Initialise le plateau par génération automatique et aléatoire à partir de Lettres.txt.
         {
-            this.matlettres = matlettres;
-            this.lettrescontraintes = lettrescontraintes;
+            Matrice = new char[nblignes, nbcolonnes];
+            lettrescontraintes = ChargerContraintesLettres();
+            GenererPlateaualeatoire();
+        }
+        public Plateau(string nomfile) : this()  //Initialise par fichier
+        {
+            ToRead(nomfile);
         }
 
-        public string[,] Matlettres
+        public char[,] Matrice
         {
-            get { return matlettres; }
+            get { return matrice; }
+            private set {; }
         }
-        public Dictionary<char, Tuple<int, int>> Lettrescontraintes
-        {
-            get { return lettrescontraintes; }
-        }
+   
         public string toString()
         {
-            return ($"matlettres : {this.matlettres} \nlettrescontraintes : {this.lettrescontraintes} ");
+            return ($"matlettres : {this.matrice} \nlettrescontraintes : {this.lettrescontraintes} ");
         }
         public void ToFile(string nomfile)
         {
 
         }
+        public void GenererPlateaualeatoire()//génère le plateau aléatoirement
+        {
+            List<char> lettresPool = new List<char>();
+
+            foreach (var kvp in lettrescontraintes)//parcours la règle de chaque lettre
+            {
+                for (int i = 0; i < kvp.Value.max; i++)//la pondération, ajoute la lettre en son nombre maximum de fois
+                {
+                    lettresPool.Add(kvp.Key);
+                }
+            }
+
+            if (lettresPool.Count == 0) { //si le nombre d'élément de la liste est nul, erreur
+            return;
+            }
+
+            for (int i = 0; i < nblignes; i++)
+            {
+                for (int j = 0; j < nbcolonnes; j++)
+                {
+                    int index = r.Next(lettresPool.Count); //choix d'un index aléatoire
+                    Matrice[i, j] = lettresPool[index];  //remplissage aléatoire de la matrice
+                }
+            }
+        }
         public void ToRead(string nomfile)
         {
 
         }
-
-        public object Recherche_Mot(string mot)
+        private Dictionary<char, (int max, int poids)> ChargerContraintesLettres()
+        {
+        }
+            public object Recherche_Mot(string mot)
         {
             return null;
         }
