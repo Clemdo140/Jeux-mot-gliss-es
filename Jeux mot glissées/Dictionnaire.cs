2c5dec59108e37cd4a2aafacb79b4669f3427281
hubertfrd <hubert.ferrand25@gmail.com>
2025-11-21 11:44:14 +0100

    l

--- Jeux mot glissées/Dictionnaire.cs
+++ Jeux mot glissées/Dictionnaire.cs
@@ -1,29 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Jeux_mot_glissées
 {
     internal class Dictionnaire
     {
-        List<List<string>> mots;
-        string langue;
+        // Définition des attributs privés de la classe dictionnaire
 
-        public Dictionnaire(List<List<string>> mots, string langue)
+        private List<string> mots; 
+        private string langue = "Français"; //Langue du dictionnaire, par défaut en "Français"
+
+        // Constructeur de la classe dictionnaire pour initialiser les attributs
+        public Dictionnaire(string MotsFrancais.txt )
         {
             this.mots = mots;
             this.langue = langue;
         }
-        public List<List<string>> Mots
+        public List<string> Mots
         {
-            get { return mots; }
+            get { return this.mots; }
         }
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public string Langue
         {
-            get { return langue; }
+            get { return this.langue; }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public string toString()
         {
--- Jeux mot glissées/Jeu.cs
+++ Jeux mot glissées/Jeu.cs
@@ -8,6 +8,39 @@ namespace Jeux_mot_glissées
 {
     internal class Jeu
     {
-       
+
+        private Dictionnaire dictionnaire;
+        private Plateau plateauCourant;
+        private List<Joueur> joueurs;
+
+        // B. Paramètres de jeu (à configurer au lancement)
+        private TimeSpan tempsParTour;  // Durée maximum allouée à chaque joueur par tour (TimeSpan)
+        private TimeSpan tempsPartieTotal; // Durée totale de la partie (TimeSpan)
+
+        // Chemin du fichier, déclaré comme constante
+        private string CHEMIN_DICTIONNAIRE = "MotsFrancais.txt";
+
+        public Jeu(TimeSpan tempsTour, TimeSpan tempsTotal)
+        {
+            // Initialisation des objets nécessaires (Contrôle Centralisé)
+
+            // 1. Initialisation du dictionnaire (déjà discutée)
+            // L'appel au constructeur lance la lecture du fichier et le tri.
+            this.dictionnaire = new Dictionnaire(CHEMIN_DICTIONNAIRE);
+
+            // 2. Initialisation de la collection de joueurs (List<T>)
+            this.joueurs = new List<Joueur>();
+
+            // 3. Initialisation des paramètres de temps
+            this.tempsParTour = tempsTour;
+            this.tempsPartieTotal = tempsTotal;
+
+            // Note : Le plateau n'est PAS initialisé ici, car son type (aléatoire ou fichier)
+            // est déterminé plus tard par le menu.
+        }
+
+
+
+
     }
 }
